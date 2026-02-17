f8b82fef6aae12f8d6e10e86e3aa18cd2b12ac1e
Muhammed Anas K <muhammedanask247@gmail.com>
2026-02-17 07:54:07 +0530

    CORS bug fixed

--- src/main/java/com/securitymak/securitymak/security/SecurityConfig.java
+++ src/main/java/com/securitymak/securitymak/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://security-mak-frontend.vercel.app"));
         config.setAllowedMethods(
             List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         );
